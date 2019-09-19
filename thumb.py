d39f8dbd80d1394d6e4bfd52fa51760d1713d8d8
trunk44 <16759830+trunk44@users.noreply.github.com>
2019-09-19 18:01:03 +0300

    Add files via upload

---
+++ thumb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# thumbnail creator
+#
+
+import os
+from PIL import Image, ExifTags
+import glob 
+
+size = 128, 128
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+files = glob.glob(dir_path + '/**/*.*', recursive=True)
+
+for infile in files:
+    if Image:
+        outfile = os.path.splitext(infile)[0] + ".thumbnail"
+        if infile != outfile:
+            try:
+                im = Image.open(infile)
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(outfile, "JPEG")
+                print("thumb", infile, "created")
+            except IOError:
+                print("cannot create thumbnail for '%s'" % infile)        
+            
