353f1f44f29e761de76ba17a7efe49c8a2cf36cd
trunk44 <16759830+trunk44@users.noreply.github.com>
2019-09-20 19:50:50 +0300

    Add files via upload

--- thumb.py
+++ thumb.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# thumbnail /w exiftags creator
+# thumbnail creator
 #
 
 import os
-from PIL import Image, ExifTags
+from PIL import Image, ExifTags, ImageFile
 import glob 
+from datetime import datetime
+
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 size = 128, 128
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 files = glob.glob(dir_path + '/**/*.*', recursive=True)
 
+startTime = datetime.now().replace(microsecond=0)
+
 for infile in files:
     if Image:
-       
-        outfile = os.path.splitext(infile)[0] + ".thumbnail"
-        if infile != outfile:
+        try:
+            im = Image.open(infile)
+            outfile = os.path.splitext(infile)[0] + ".thumbnail"
             try:
-                im = Image.open(infile)
-                exif = im.info['exif']
+                im.info['exif']
                 im.thumbnail(size, Image.ANTIALIAS)
-                im.save(outfile, "JPEG",  exif=exif)
-                print("thumb", infile, "created")
-            except IOError:
-                print("cannot create thumbnail for '%s'" % infile)        
-            
+                im.save(outfile, "JPEG", exif=im.info['exif'])
+                print("thumb", infile, "created: jpg")
+            except:
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(outfile, "PNG")
+                print("thumb", infile, "created: png")
+        except IOError:
+            print("cannot create thumbnail for '%s'" % infile) 
+                
+                
+endTime = datetime.now().replace(microsecond=0) 
+print ('\nTime used: ', endTime - startTime)
