05dd0e5e768d1561f77e4bff1dadfe078461913b
trunk44 <16759830+trunk44@users.noreply.github.com>
2019-09-21 17:44:26 +0300

    Add files via upload

--- thumb-multi.py
+++ thumb-multi.py
@@ -17,33 +17,34 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def fn(infile):
-    if Image:
+    outfile = os.path.splitext(infile)[0] + ".thumbnail"
+    if infile != outfile:
+        try:
+            im = Image.open(infile)
             try:
-                im = Image.open(infile)
-                outfile = os.path.splitext(infile)[0] + ".thumbnail"
-                try:
-                    im.info['exif']
-                    im.thumbnail(size, Image.ANTIALIAS)
-                    im.save(outfile, "JPEG", exif=im.info['exif'])
-                    print("thumb", infile, "created:j")
-                except:
-                    im.thumbnail(size, Image.ANTIALIAS)
-                    im.save(outfile, "PNG")
-                    print("thumb", infile, "created: png")
-            except IOError:
-                print("cannot create thumbnail for '%s'" % infile)   
-    else:
-        pass
+                im.info['exif']
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(outfile, "JPEG", exif=im.info['exif'])
+                print("thumbnail created for: '%s'." % outfile,)
+            except:
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(outfile, "PNG")
+                print("thumbnail created for: '%s'." % outfile,)
+        except IOError:
+            print("cant create thumb for: '%s'" % infile)
+            pass
+
                 
              
 if __name__ == '__main__':
     cpucores = multiprocessing.cpu_count()
     files = glob.glob(dir_path + '/**/*.*', recursive=True)
     startTime = datetime.now().replace(microsecond=0)
-    pool = Pool(processes=cpucores + 1)
+    pool = Pool(processes=cpucores + 1) #cpu cores + 1 : best known result
+    
     pool.map(fn, files)
     endTime = datetime.now().replace(microsecond=0) 
 
-    print ('\nTime used: ', endTime - startTime)
+    print('\nTime used: ', endTime - startTime)
     print('\nCPU Cores: ', cpucores)
-    print('\nFlows used: ', cpucores + 1 )
+    print('\nFlows used: ', cpucores + 1)
