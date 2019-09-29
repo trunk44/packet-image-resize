551c421683974bdf03db53f096b084464d6e7c9c
trunk44 <16759830+trunk44@users.noreply.github.com>
2019-09-29 17:38:45 +0300

    Add files via upload

--- tab.py
+++ tab.py
@@ -1,25 +1,45 @@
+from datetime import datetime
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import multiprocessing
+from multiprocessing import Pool
+import os
+
 
 from html_table_parser import HTMLTableParser
  
 url = 'https://bases-brothers.ru/categories.aspx?parent='
-
+URLS = 'urls.txt'
+TABLES = 'tables.txt'
 
 class HTMLTableParser:
 
-    def parse_url(self, url):
+    def parse_urltable(self, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
-        return [(table['id'],self.parse_html_table(table))\
-                for table in soup.find_all('table')]  
+        return [(table['id'],self.parse_htmltable(table))\
+                for table in soup.find_all('table')]
 
-    def parse_html_table(self, table):
+    def parse_urldiv(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        divtext = [soup.find('div', {'class':'BreadCrumbs'})] # Parse the div by div class.
+        city = [soup.find('span', text='Все объявления в Москве')]# Parse the span.
+        print(city)
+        
+        f = open(TABLES, 'a')
+        f.write("%s\n" % divtext)
+        f.close()
+        return divtext
+    
+    def parse_htmltable(self, table):
+        
         n_columns = 0
-        n_rows=0
+        n_rows = 0
         column_names = []
 
         # Find number of rows and columns
@@ -48,46 +68,93 @@ class HTMLTableParser:
         df = pd.DataFrame(columns = columns,
                           index= range(0,n_rows))
         row_marker = 0
+        
         for row in table.find_all('tr'):
             column_marker = 0
             columns = row.find_all('td')
-            for column in columns:
-                df.iat[row_marker,column_marker] = column.get_text()
-                column_marker += 1
-            if len(columns) > 0:
-                row_marker += 1
-
+            if len(columns) == 4: #cut yandex and other shit (!= 4) rows here
+                if row_marker != 24 and row_marker != 49: #cut some empty lines here
+                    rowtext = [i.text for i in columns]
+                    f = open(TABLES, 'a')
+                    f.write("%s\n" % rowtext)
+                    # f.close() moved to end of func.
+                else:
+                    pass
+                 
+                for column in columns:
+                    df.iat[row_marker,column_marker] = column.get_text()
+                    column_marker += 1
+                    
+                if len(columns) > 0:
+                    row_marker += 1
+                    
+            else:
+                pass
+                
         # Convert to float if possible
         for col in df:
             try:
                 df[col] = df[col].astype(float)
             except ValueError:
                 pass
-
+            
+        f.close()
         return df
+    
+
+    def writetable(self, url):
+        i = 0
+        try:
+            
+            table = self.parse_urltable(url)[0][1] # Grabbing the table from the tuple
+            print(table.head())
+            # only if table is passed, then try to grab htmls div:
+            print(self.parse_urldiv(url))
+            i += 1
+            
+        except:
+            print('pass')
+            i += 1
+            pass
+        
+        return i
+
+#Чтение из файла
+def readlinks(file):
+    with open(createif(file)) as f:
+        lines = f.read().splitlines()
+    return lines
+
+#Проверка файла на существование
+def checkfile(file):
+    return os.path.isfile(file)
+
+#Создание файла если нет
+def createif(file):
+    if checkfile(file):
+        return file
+    else:
+        open(file, 'a').close()
+        print('Файл ' + file + ' был создан')
+        return file
+    
+#Добавление в файл
+def addlinks(file,my_list):
+    with open(createif(file), 'a') as f:
+        for item in my_list:
+            f.write("%s\n" % item)
 
 def main():
     hp = HTMLTableParser()
-    i = 0
+    cpucores = multiprocessing.cpu_count()
+    pool = Pool(processes=cpucores + 4)
+    urlpool = [url + str(i) for i in range (1, 8)] # pool of urls
     
-    while True:
-        try:
-            i += 1
-            nurl = url + str(i)
-            print(nurl)
-            try:
-                table = hp.parse_url(nurl)[0][1] # Grabbing the table from the tuple
-                #return table
-                print(table.head())
-                table = []
-            except:
-                pass
-
-            
-            #print(table)
-        except:
-            print('pass')
-            pass
-
+    startTime = datetime.now().replace(microsecond=0)
+    pool.map(hp.writetable, urlpool)
+    endTime = datetime.now().replace(microsecond=0)
+    
+    print('\nTime used: ', endTime - startTime)
+    
 if __name__ == "__main__":
     main()
